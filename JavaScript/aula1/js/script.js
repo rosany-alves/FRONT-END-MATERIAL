7856ea4bb9739f30b9ca34cf0e38805657b2e0ea
Rosany <104765832+rosany-alves@users.noreply.github.com>
2024-04-18 09:56:39 -0300

    javascript
    aula 18/04/2024

---
+++ JavaScript/aula1/js/script.js
@@ -0,0 +1,12 @@
+console.log("Olá mundo")
+
+//caixas de texto (input)
+//prompt = recebe um texto
+//window.prompt("Qual é seu nome?");
+//alert = manda uma mensagem
+//window.alert("FRONT É TOP!");
+//confirm = pergunta podendo sair verdadeiro ou falso
+//Window.confirm("Deseja baixar arquivo novamente?")
+
+var melancia = "Uma variavel de teste"/
+var ligado = true;
